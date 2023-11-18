72404bc7041e5050dd973d98ee38799ece9ca77e
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-11-18 09:58:25 +0100

    examples: simplify astro-project example

--- examples/astro-project/src/main.v
+++ examples/astro-project/src/main.v
@@ -12,22 +12,24 @@ fn main() {
 	)
 	w.set_title('Astro + V + webview')
 	w.set_size(800, 600, .@none)
+
 	// Bind V functions.
 	w.bind_opt[voidptr]('__open', ui.open)
 	w.bind_opt('__increment', increment)
-	// Serve UI.
+
+	// Serve the UI.
 	ui_path := os.join_path(@VMODROOT, 'ui')
 	$if dev ? {
-		w.set_icon(os.join_path(ui_path, 'public', 'favicon.svg'))!
 		w.serve_dev(ui_path)!
 		// w.serve_dev(ui_path, pkg_manager: .yarn)! // `w.serve_dev(ui_path)!` works with `yarn` as well.
 		// w.serve_dev(ui_path, pkg_manager: .pnpm)!
 	} $else {
 		build_path := os.join_path(ui_path, 'dist')
-		w.set_icon(os.join_path(build_path, 'favicon.svg'))!
 		w.serve_static(build_path)!
 	}
+
 	// Run and wait until the window gets closed.
 	w.run()
+	// Detroy the window, in case of `-d dev` end npm process, clear up resources.
 	w.destroy()
 }
